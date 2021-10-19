0555f26f838ac83bc8f2d7ffb51c8c9d57fc1c0e
Hugo Lamounier <hugo.lamounier@dtidigital.com.br>
2021-10-19 09:57:29 -0300

    Changes

--- EntityFramework/Controllers/ExampleController.cs
+++ EntityFramework/Controllers/ExampleController.cs
@@ -1,6 +1,8 @@
 ﻿using EntityFramework.Adapter;
 using EntityFramework.Models;
+using EntityFramework.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -23,8 +25,55 @@ namespace EntityFramework.Controllers
         [HttpGet]
         public IEnumerable<Book> Get()
         {
-
             return _mySqlDatabaseContext.Books.Include("Author").ToList();
+        }
+
+        [HttpPost]
+        [Route("TransactionTestRollback")]
+        public async Task<IActionResult> TransactionTestRollback([FromBody] Book book)
+        {
+
+            using (var transaction = _mySqlDatabaseContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var response = await _mySqlDatabaseContext.AddAsync<Book>(book);
+                    transaction.Commit();
+
+                    return Ok(response);
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return BadRequest("Transaction not commited");
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("TransactionTestCommit")]
+        public async Task<IActionResult> TransactionTestCommit([FromBody] InsertBookRequestDto bookRequest)
+        {
+
+            using (var transaction = _mySqlDatabaseContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var authorCreated = await _mySqlDatabaseContext.AddAsync(bookRequest.Author);
+
+                    bookRequest.Book.Author = authorCreated.Entity;
+
+                    var response = await _mySqlDatabaseContext.AddAsync(bookRequest.Book);
+                    transaction.Commit();
+
+                    return Ok(response);
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return BadRequest("Transaction not commited");
+                }
+            }
         }
     }
 }
---
+++ EntityFramework/Models/Dto/InsertBookRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Models.Dto
+{
+    public class InsertBookRequestDto
+    {
+        public Author Author { get; set; }
+        public Book Book { get; set; }
+    }
+}
